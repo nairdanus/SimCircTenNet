08e6b1586c6f8a42597dcab4e516252b397b719a
adrian <nairdanus@gmail.com>
2024-04-21 23:16:31 +0200

    Added method to write LinAlg error if it occurs

--- MPS/simulator.py
+++ MPS/simulator.py
@@ -1,4 +1,5 @@
 import random
+import pickle
 
 import numpy as np
 import tensornetwork as tn
@@ -486,8 +487,13 @@ class MPS_Simulator:
             self.real_𝓧s.append(max(u.tensor.shape)+len(trun_vals))
 
         except np.linalg.LinAlgError as LinAlgError:
+            with open("LINALG_ERROR.pkl", "w") as err_file:
+                pickle.dump((node1, node2, uu, new_node, l, r), file=err_file)
             print("Following tensor did not converge:")
             print(new_node.tensor)
+            print("Min: ", new_node.tensor.min())
+            print("Max: ", new_node.tensor.max())
+            print("NaN at indices: ", np.argwhere(np.isnan(new_node.tensor)))
             raise LinAlgError
 
         self.network[ctl] = u
